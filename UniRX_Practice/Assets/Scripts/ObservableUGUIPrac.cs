554db89c81fea42279d3ad15c1d20ac3fdd96d84
tyrosine1153 <tysine1153@gmail.com>
2021-09-02 15:56:20 +0900

    Add practice scripts
    about ugui

---
+++ UniRX_Practice/Assets/Scripts/ObservableUGUIPrac.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObservableUGUIPrac : MonoBehaviour
+{
+    [SerializeField] private Button button;
+    [SerializeField] private InputField inputField;
+    [SerializeField] private Slider slider;
+    [SerializeField] private Text text1;
+    [SerializeField] private Text text2;
+    
+    void Start()
+    {
+        button.OnClickAsObservable().Subscribe();
+
+        inputField.OnValueChangedAsObservable()./*Where(msg => msg == "정답").*/Subscribe(msg => text1.text = msg);
+        inputField.OnEndEditAsObservable().Subscribe();
+
+        slider.OnValueChangedAsObservable()./*Where(f => f <= 0.7 && f >= 0.4).*/Subscribe(f=>text2.text = $"{f}");
+    }
+}
