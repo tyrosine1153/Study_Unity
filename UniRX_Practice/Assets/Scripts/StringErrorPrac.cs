0acfeecc8e370836d7e5f761238630722b5448ce
tyrosine1153 <tyrosine1153@gmail.com>
2021-09-10 19:24:47 +0900

    Add practice examples (Error Exception) - UniRX

---
+++ UniRX_Practice/Assets/Scripts/StringErrorPrac.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+public class StringErrorPrac : MonoBehaviour
+{
+    private Subject<string> _subject = new Subject<string>();
+
+    private void Start()
+    {
+        _subject.Select(str => int.Parse(str))
+            .OnErrorRetry((FormatException ex) =>
+            {
+                Debug.Log("FormatException으로 Stream 재구축");
+            })
+            .Subscribe(x => Debug.Log($"성공 : {x}"), 
+                x => Debug.Log($"실패 : {x}"));  // FormatException일 때, 실패 부분은 실행되지 않음
+        
+        _subject.OnNext("1");
+        _subject.OnNext("2");
+        _subject.OnNext("Hello!");
+        _subject.OnNext("4");
+        _subject.OnNext("5");
+    }
+}
