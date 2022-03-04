573b0280ef2c7d59c9464829b413c459e01ece26
Yu Jeongmin <tyrosine1153@gmail.com>
2022-03-04 15:43:49 +0900

    Add editor examples (AssetDatabase, Editor copy) - Etc Practice

---
+++ Etc_Practice/Assets/Script/Editor/AssetDatabaseTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AssetDatabaseTest : EditorWindow
+{
+    [MenuItem("Window/Editor Practice/Asset Database Test")]
+    public static void ShowWindow()
+    {
+        GetWindow<AssetDatabaseTest>("Asset Database Test");
+    }
+    
+    private void OnGUI()
+    {
+        if (GUILayout.Button("모든 Material 찾기"))
+        {
+            var guids = AssetDatabase.FindAssets("t:Material");
+            
+            foreach (var guid in guids)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                Debug.Log($"GUID : {guid}, Path : {path}, GUID from Path : {AssetDatabase.AssetPathToGUID(path)}");
+                Debug.Log("===============================");
+            }
+        }
+
+        if (GUILayout.Button("Material Asset 생성하기"))
+        {
+            var material = new Material(Shader.Find("Standard"));
+            AssetDatabase.CreateAsset(material, $"Assets/Resources/EditorTest/MyMaterial{Random.Range(0, 1000)}.mat");
+        }
+        
+        if (GUILayout.Button("모든 Material 로드 및 적용"))
+        {
+            var renderers = FindObjectsOfType<Renderer>();
+            foreach (var renderer in renderers)
+            {
+                DestroyImmediate(renderer.gameObject);
+            }
+            
+            var guids = AssetDatabase.FindAssets("t:Material");
+
+            for (int i = 0; i < guids.Length; i++)
+            {
+                var guid = guids[i];
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                
+                var material = AssetDatabase.LoadAssetAtPath<Material>(path);
+                if (material != null)
+                {
+                    Debug.Log($"Material Loaded : {path}");
+
+                    var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+                    cube.transform.position = new Vector3(i * 2, 0, 0);
+                    cube.GetComponent<Renderer>().material = material;
+                }
+            }
+        }
+    }
+}
---
+++ Etc_Practice/Assets/Script/Editor/EditorCopy.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EditorCopy : EditorWindow
+{
+    
+    private Editor duplicatedEditor;  // 복제한 오브젝트 표시 에디터
+    private Editor[] duplicatedEditorDetails;  // 복제한 컴포넌트 표시 에디터
+    private List<bool> detailFoldout = new List<bool>();  // 토글 상태 저장
+
+    private string stringvalue = "";
+    private int intvalue = 0;
+
+    [MenuItem("Window/Editor Practice/Editor Copy")]
+    public static void ShowWindow()
+    {
+        GetWindow<EditorCopy>("Editor Copy");
+    }
+
+    private void OnGUI()
+    {
+        if (Selection.objects != null && Selection.objects.Length == 1)
+        {
+            var target = Selection.objects[0];
+            if (duplicatedEditor == null || duplicatedEditor.name != target.name)
+            {
+                duplicatedEditor = Editor.CreateEditor(target);
+                var gameObject = target as GameObject;
+                if (gameObject != null)
+                {
+                    var components = gameObject.GetComponents<Component>();
+                    if (components != null)
+                    {
+                        duplicatedEditorDetails = new Editor[components.Length];
+                        for (int i = 0; i < components.Length; i++)
+                        {
+                            duplicatedEditorDetails[i] = Editor.CreateEditor(components[i]);
+                            detailFoldout.Add(false);
+                        }
+                    }
+                }
+                else
+                {
+                    duplicatedEditorDetails = null;
+                    detailFoldout.Clear();
+                }
+            }
+        }
+        else
+        {
+            duplicatedEditor = null;
+            duplicatedEditorDetails = null;
+            detailFoldout.Clear();
+        }
+
+        if (duplicatedEditor != null)
+        {
+            duplicatedEditor.DrawHeader();
+            duplicatedEditor.OnInspectorGUI();
+
+            if (duplicatedEditorDetails != null)
+            {
+                for (int i = 0; i < duplicatedEditorDetails.Length; i++)
+                {
+                    // Fold Out : 토글같은 거
+                    detailFoldout[i] = EditorGUILayout.Foldout(detailFoldout[i], duplicatedEditorDetails[i].GetType().ToString());
+                    if (detailFoldout[i])
+                    {
+                        duplicatedEditorDetails[i].OnInspectorGUI();
+                    }
+                }
+            }
+        }
+    }
+}
